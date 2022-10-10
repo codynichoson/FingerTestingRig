4f38bb15b91d52420a34835b48881bae454a610c
Cody Nichoson <codynichoson@gmail.com>
2022-10-10 17:24:57 -0500

    Edited vision node to print out image topic characteristics

--- finger_rig_control/src/vision.cpp
+++ finger_rig_control/src/vision.cpp
@@ -1,16 +1,33 @@
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+using std::placeholders::_1;
 
 class Vision : public rclcpp::Node
 {
-    public:
-        Vision() : Node("vision") {}
-    private:
+  public:
+    // Constructor
+    Vision() : Node("minimal_subscriber")
+    {
+      image_sub_ = this->create_subscription<sensor_msgs::msg::Image>("/camera/image", 10, std::bind(&Vision::topic_callback, this, _1));
+    }
+
+  private:
+    void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(),
+        "Right Rectified image received from ZED\tSize: %dx%d - Timestamp: %u.%u sec ",
+        msg->width, msg->height,
+        msg->header.stamp.sec,msg->header.stamp.nanosec);
+    }
+    
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
 };
 
-int main(int argc, char **argv)
+int main(int argc, char * argv[])
 {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<Vision>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Vision>());
+  rclcpp::shutdown();
+  return 0;
 }
